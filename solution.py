95d015a43afa3f319ea2b302c7968b40a2bc52ba
barnastik <113926737+barnastik@users.noreply.github.com>
2023-04-07 15:47:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1004689536 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    import math
+    n = len(x)
+    ln_x = [math.log(i) for i in x]
+    a = math.exp(1/n * sum(ln_x) - math.log(431))
+    return a # Ваш ответ
